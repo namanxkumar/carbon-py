d1e80fd84d4f1c2170dd9e19ef797dfc45658d62
Naman Kumar <namanxkumar@gmail.com>
2025-07-13 01:52:04 +0530

    cleanup main

--- main.py
+++ main.py
@@ -21,24 +21,24 @@ execution_graph = ExecutionGraph(robot)
 
 
 print(robot.get_description())
-print("\nExecution Graph Layers:")
-print(execution_graph.layers)
-print("\nProcess Groups:")
-for process_index, process in execution_graph.processes.items():
-    print(f"Process {process_index}:")
-    for method in process:
-        print(
-            f"  {method.name} (depends on: {method.dependencies}, produces: {method.dependents})"
-        )
-print("\nConnections:")
-for connection in robot.get_connections():
-    print(connection)
-print("\nMethods:")
-for method in robot.get_methods():
-    print(method.name)
-    print("  Depends on:", method.dependents)
-    print("  Produces for:", method.dependencies)
+# print("\nExecution Graph Layers:")
+# print(execution_graph.layers)
+# print("\nProcess Groups:")
+# for process_index, process in execution_graph.processes.items():
+#     print(f"Process {process_index}:")
+#     for method in process:
+#         print(
+#             f"  {method.name} (depends on: {method.dependencies}, produces: {method.dependents})"
+#         )
+# print("\nConnections:")
+# for connection in robot.get_connections():
+#     print(connection)
+# print("\nMethods:")
+# for method in robot.get_methods():
+#     print(method.name)
+#     print("  Depends on:", method.dependents)
+#     print("  Produces for:", method.dependencies)
+print()
+execution_graph.execute()
 print()
 print("\nExecution completed.")
-
-execution_graph.execute()
